d4abb80c1b6020fbea2971358d2f86f82c5d8a3d
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-05-20 02:49:29 -0400

    Make the 'messages' field in User readable

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -76,6 +76,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     /**
      * @ORM\OneToMany(targetEntity=Message::class, mappedBy="user")
+     * @Groups("user:read")
      */
     private $messages;
 
