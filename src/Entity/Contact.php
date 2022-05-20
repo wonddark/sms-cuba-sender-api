54d6d7280324bb47d2270358bec4bb9cf086d898
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-05-20 02:39:18 -0400

    Created entity Contacts
    -- Linked to User as many contacts belong to only one user
    -- Generated database schema migration

---
+++ src/Entity/Contact.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\ContactRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ApiResource(
+ *     collectionOperations={
+ *          "get", "post"
+ *     },
+ *     itemOperations={
+ *          "get", "patch", "delete"
+ *     }
+ * )
+ * @ORM\Entity(repositoryClass=ContactRepository::class)
+ */
+class Contact
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups("user:read")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=8)
+     * @Groups("user:read")
+     */
+    private $phone;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="contacts")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
